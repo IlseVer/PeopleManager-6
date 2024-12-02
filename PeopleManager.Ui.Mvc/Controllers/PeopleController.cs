27caa84a92be62e2f564bdaa66b3d1cbec1399f4
IlseVer <ilse.verhaeghe@student.vives.be>
2024-12-02 13:32:42 +0100

    Added forms validation for Person

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -2,105 +2,106 @@
 using PeopleManager.Ui.Mvc.Core;
 using PeopleManager.Ui.Mvc.Models;
 
-namespace PeopleManager.Ui.Mvc.Controllers
+namespace PeopleManager.Ui.Mvc.Controllers;
+
+public class PeopleController : Controller
 {
-    public class PeopleController : Controller
+    private readonly PeopleManagerDbContext _peopleManagerDbContext;
+
+    public PeopleController(PeopleManagerDbContext peopleManagerDbContext)
     {
+        _peopleManagerDbContext = peopleManagerDbContext;
+    }
 
-        private readonly PeopleManagerDbContext _peopleManagerDbContext;
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var people = _peopleManagerDbContext.People.ToList();
 
-        public PeopleController(PeopleManagerDbContext peopleManagerDbContext)
-        {
-            _peopleManagerDbContext = peopleManagerDbContext;
-        }
+        return View(people);
+    }
 
-		[HttpGet]
-		public IActionResult Index()
-        {
-            var people = _peopleManagerDbContext.People.ToList();
+    // CREATE
+    // GET
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-            return View(people);
-        }
-		// CREATE
-		// GET
-		[HttpGet]
-		public IActionResult Create()
-		{
+    // POST --> data verwerken
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Person person)
+    {
+        if (!ModelState.IsValid) return View(person);
+        _peopleManagerDbContext.People.Add(person);
+        _peopleManagerDbContext.SaveChanges(); //zeker doen, nu moet je het uitvoeren
+        return RedirectToAction("Index");
+    }
 
-			return View();
-		}
-		// POST --> data verwerken
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public IActionResult Create(Person person)
-		{
-				_peopleManagerDbContext.People.Add(person);
-				_peopleManagerDbContext.SaveChanges();  //zeker doen, nu moet je het uitvoeren
-				return RedirectToAction("Index");
-		}
-
-        // EDIT
-        // GET
-        [HttpGet]
-        public IActionResult Edit([FromRoute]int id)
-        {
-            var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id); //als hij de persoon niet vindt, dan is het null
-            if (person is null)  //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
-            {
-                return RedirectToAction("Index");
-            }
-            return View(person);
-        }
-
-        // POST  --> data verwerken
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit([FromRoute]int id, [FromForm]Person person)// id moet je van de route halen, person uit het formulier
-        {
-            var dbPerson = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id); //als hij de persoon niet vindt, dan is het null
-            if (dbPerson is null)  //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
-            {
-                return RedirectToAction("Index");
-            }
-            dbPerson.FirstName = person.FirstName;
-            dbPerson.LastName = person.LastName;
-            dbPerson.Email = person.Email;
-
-            _peopleManagerDbContext.SaveChanges();  //zeker doen, nu moet je het uitvoeren
-
+    // EDIT
+    // GET
+    [HttpGet]
+    public IActionResult Edit([FromRoute] int id)
+    {
+        var person =
+            _peopleManagerDbContext.People.FirstOrDefault(p =>
+                p.Id == id); //als hij de persoon niet vindt, dan is het null
+        if (person is null) //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
             return RedirectToAction("Index");
-        }
+        return View(person);
+    }
+
+    // POST  --> data verwerken
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult
+        Edit([FromRoute] int id, [FromForm] Person person) // id moet je van de route halen, person uit het formulier
+    {
+        if (!ModelState.IsValid) return View(person);
+        var dbPerson =
+            _peopleManagerDbContext.People.FirstOrDefault(p =>
+                p.Id == id); //als hij de persoon niet vindt, dan is het null
+        if (dbPerson is null) //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
+            return RedirectToAction("Index");
+        dbPerson.FirstName = person.FirstName;
+        dbPerson.LastName = person.LastName;
+        dbPerson.Email = person.Email;
+
+        _peopleManagerDbContext.SaveChanges(); //zeker doen, nu moet je het uitvoeren
+
+        return RedirectToAction("Index");
+    }
 
 
-        [HttpGet]
-        public IActionResult Delete([FromRoute] int id)
+    [HttpGet]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        var person =
+            _peopleManagerDbContext.People.FirstOrDefault(p =>
+                p.Id == id); //als hij de persoon niet vindt, dan is het null
+        if (person is null) //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
+            return RedirectToAction("Index");
+        return View(person);
+    }
+
+    [HttpPost]
+    [Route("People/delete/{id:int?}")] //dit is de route die je moet volgen id:int --> dubbele validate- het moet een getal zijn
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        var person = new Person
         {
-	        var person = _peopleManagerDbContext.People.FirstOrDefault(p => p.Id == id); //als hij de persoon niet vindt, dan is het null
-	        if (person is null)  //indien null (zo weinig mogelijk een else schrijven) //alle logica vanboven
-	        {
-		        return RedirectToAction("Index");
-	        }
-	        return View(person);
-        }
+            Id = id,
+            FirstName = string.Empty,
+            LastName = string.Empty
+        };
 
-		[HttpPost]
-        [Route("People/delete/{id:int?}")] //dit is de route die je moet volgen id:int --> dubbele validate- het moet een getal zijn
-        [ValidateAntiForgeryToken]
-		public IActionResult DeleteConfirmed(int id)
-        {
-            var person = new Person
-            {
-                Id = id,
-                FirstName = string.Empty,
-                LastName = string.Empty,
-            };
+        _peopleManagerDbContext.People.Attach(person);
+        _peopleManagerDbContext.People.Remove(person); //markeren als je mag die verwijderen
+        _peopleManagerDbContext.SaveChanges(); //het effectief verwijderen
 
-            _peopleManagerDbContext.People.Attach(person); 
-            _peopleManagerDbContext.People.Remove(person); //markeren als je mag die verwijderen
-			_peopleManagerDbContext.SaveChanges();  //het effectief verwijderen
-
-			return RedirectToAction("Index"); 
-		}
-
-	}
+        return RedirectToAction("Index");
+    }
 }
--- PeopleManager.Ui.Mvc/Models/Person.cs
+++ PeopleManager.Ui.Mvc/Models/Person.cs
@@ -1,13 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PeopleManager.Ui.Mvc.Models
 {
     [Table(nameof(Person))]
     public class Person
     {
-        public int Id { get; set; }
-        public required string FirstName { get; set; }
+        public int Id { get; set; } // Primary Key
+
+        [Required] // Required voor UI
+        [DisplayName("First Name")] // ales mijn naam van mijn property gerenderd moet worden op het scherm
+        [StringLength(100)] // max 50 characters
+        public required string FirstName { get; set; } //required voor back-end
+
+        [Required]
+        [DisplayName("Last Name")]
         public required string LastName { get; set; }
+
+        [DisplayName("E-mail address")]
+        [EmailAddress]
         public string? Email { get; set; }
     }
 }
